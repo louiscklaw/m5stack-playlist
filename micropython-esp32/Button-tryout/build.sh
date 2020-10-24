66fa077b2d0f6cf82bbdfb93bf46911c9497c199
louiscklaw <louiscklaw@gmail.com>
2020-10-25 01:19:01 +0800

    update,

--- micropython-esp32/Button-tryout/build.sh
+++ micropython-esp32/Button-tryout/build.sh
@@ -2,13 +2,37 @@
 
 set -ex
 
-IMG_DIR=$MICRO_PYTHON_BUILD_DIR/components/internalfs_image/image
+SRC_DIR=$PWD
 
-rsync -avzh \
-$PWD/*.py \
-$IMG_DIR
+# IMG_DIR=$MICRO_PYTHON_BUILD_DIR/components/internalfs_image/image
 
-# flash to device
-$PWD/../spiffs_image-tryout/test.sh
+# cd $MICRO_PYTHON_BUILD_DIR
+#   ./BUILD.sh makefs
+# cd -
 
-code $PWD/main.py
+# rsync -avzh \
+# $PWD/*.py \
+# $IMG_DIR
+
+# # flash to device
+# $PWD/../spiffs_image-tryout/test.sh
+
+# code $PWD/main.py
+
+cd $PROJ_HOME
+  scripts/0_clean.sh
+
+  # cp $SRC_DIR/sdkconfig $ACTIVE_SDKCONFIG
+  # scripts/1_menuconfig.sh
+
+  scripts/2_build_the_firmware.sh
+
+  # sync my code before generate fs image
+  rsync -avzh \
+    $SRC_DIR/*.py \
+    $FS_IMG_DIR
+
+  scripts/3_flash_the_file_system_image.sh
+
+  # download done, backup the active sdkconfig file
+  cp $ACTIVE_SDKCONFIG $SRC_DIR/sdkconfig
