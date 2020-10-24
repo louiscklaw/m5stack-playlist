06359b62bef91d26edd289bf3ad5f99729256fd6
louiscklaw <louiscklaw@gmail.com>
2020-10-25 02:24:19 +0800

    update,

--- micropython-esp32/web-tryout/main.py
+++ micropython-esp32/web-tryout/main.py
@@ -1,3 +1,48 @@
+from m5stack import lcd
+lcd.print('hello world!\n')
+
+import machine, network, utime
+
+
+lcd.print("")
+lcd.print("Starting WiFi ...")
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+
+sta_if.connect("logic_debug",'ohzei7eexaimo3Ae')
+tmo = 50
+while not sta_if.isconnected():
+    lcd.print('retrying connect \n')
+    utime.sleep_ms(1000)
+    tmo -= 1
+    if tmo == 0:
+        sta_if.disconnect()
+        break
+
+if tmo > 0:
+    lcd.print("WiFi started")
+    utime.sleep_ms(500)
+    lcd.print(sta_if.ifconfig())
+
+    rtc = machine.RTC()
+    lcd.print("Synchronize time from NTP server ...")
+    rtc.ntp_sync(server="hr.pool.ntp.org")
+    tmo = 100
+    while not rtc.synced():
+        utime.sleep_ms(100)
+        tmo -= 1
+        if tmo == 0:
+            break
+
+    if tmo > 0:
+        lcd.print("Time set")
+        utime.sleep_ms(500)
+        t = rtc.now()
+        utime.strftime("%c")
+        lcd.print(utime.strftime("%c"))
+        lcd.print('\n')
+
+
 
 from    microWebSrv import MicroWebSrv
 import _thread
