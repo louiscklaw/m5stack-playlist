224850ab7b83c1249ca706296b9924fe979cc35a
louiscklaw <louiscklaw@gmail.com>
2020-10-28 17:30:58 +0800

    update and test for curl,

---
+++ micropython-esp32/curl-tryout/main.py
@@ -0,0 +1,8 @@
+from m5stack import lcd
+
+import wifi_example
+
+import curl
+curl.get('http://www.example.com')
+
+lcd.print('initialize done\n')
---
+++ micropython-esp32/curl-tryout/wifi_example.py
@@ -0,0 +1,29 @@
+from m5stack import lcd
+import machine, network, utime
+
+lcd.print("")
+lcd.print("Starting WiFi ...")
+
+sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+sta_if.connect("logic_debug",'ohzei7eexaimo3Ae')
+tmo = 50
+while not sta_if.isconnected():
+  utime.sleep_ms(100)
+  tmo -= 1
+  if tmo == 0:
+    sta_if.disconnect()
+    break
+
+if tmo > 0:
+  utime.sleep_ms(500)
+  lcd.print('IP address : ')
+  lcd.print(sta_if.ifconfig()[0])
+  lcd.print('\n')
+
+  lcd.print("WiFi started")
+  lcd.print('\n')
+
+lcd.print('wifi initialize done...\n')
+
+def helloworld_wifi():
+  lcd.print('helloworld wifi')
